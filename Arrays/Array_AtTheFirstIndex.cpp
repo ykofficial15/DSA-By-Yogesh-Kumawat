9ac31fe98e455ba2e3e5cbd99bcc63fc5ccff952
ykofficial15 <yogesh15kumawat@gmail.com>
2024-01-09 18:46:06 +0530

    learned array insertion at the first index

---
+++ Arrays/Array_AtTheFirstIndex.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int s;
+    cout<<"enter array size"<<endl;
+    cin>>s;
+    int arr[s];
+    cout<<"enter "<<s<<" elements in the array"<<endl;
+    for(int i=0;i<s;i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<"now the array will be"<<endl;
+      for(int i=0;i<s;i++)
+    {
+        cout<<arr[i]<<endl;
+    }
+    int val;
+    cout<<"enter value that you want to enter in the first index"<<endl;
+    cin>>val;
+    for(int i=s;i>=0;i--)
+    {
+           arr[i]=arr[i-1];
+    }
+    arr[0]=val;
+    cout<<"now the final array will be"<<endl;
+    for(int i=0;i<=s;i++)
+    {
+        cout<<arr[i]<<endl;
+    }
+    return 0;
+}
