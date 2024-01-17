0b39a5406a92d18122cc178b058d29f9518d4485
ykofficial15 <yogesh15kumawat@gmail.com>
2024-01-17 22:24:29 +0530

    linkedlist searching

---
+++ LinkedList/Linkedlist_Searching.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+struct node{
+    int data;
+    struct node *next;
+};
+
+void traversal(struct node *ptr){
+    while(ptr!=NULL)
+    {
+        cout<<ptr->data<<" ";
+        ptr=ptr->next;
+    }
+}
+
+void searching(struct node *ptr,int key)
+{
+    int index=0;
+    while(ptr!=NULL)
+    {
+        if(ptr->data==key)
+        {
+            cout<<"Key "<<key<<" found at index: "<<index<<endl;
+            break;
+        }
+        else{
+            index++;
+            ptr=ptr->next;
+        }
+    }
+    
+}
+
+int main(){
+    struct node *head=(struct node*)malloc(sizeof(struct node));
+    struct node *first=(struct node*)malloc(sizeof(struct node));
+    struct node *second=(struct node*)malloc(sizeof(struct node));
+    head->data=10;
+    first->data=20;
+    second->data=30;
+    head->next=first;
+    first->next=second;
+    second->next=NULL;
+    cout<<"Linkedlist will be"<<endl;
+    traversal(head);
+    cout<<endl;
+    searching(head,20);
+    return 0;
+}
