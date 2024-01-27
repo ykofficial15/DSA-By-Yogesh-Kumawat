d88f38f39c6b31644b7f77e412367a736d1eea63
ykofficial15 <yogesh15kumawat@gmail.com>
2024-01-27 23:45:16 +0530

    stack push operation done

---
+++ Stack/Stack_Pop.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+struct node{
+  int size;
+  int *arr;
+  int top;
+};
+
+int isEmpty(struct node *ptr)
+{
+  if(ptr->top==-1)
+  {
+    cout<<"stack is empty"<<endl;
+    return 1;
+  }
+  else{
+    return 0;
+  }
+}
+
+int isFull(struct node *ptr)
+{
+  if(ptr->top==ptr->size-1)
+  {
+    return 1;
+  }
+  else{
+    return 0;
+  }
+}
+
+void push(struct node *ptr, int data)
+{
+    if(isFull(ptr))
+    {
+        cout<<"stack is overflow can't push"<<endl;
+    }
+    else{
+        ptr->top++;
+        ptr->arr[ptr->top]=data;
+        cout<<ptr->arr[ptr->top]<<" data pushed "<<endl;
+    }
+}
+
+int main(){
+  struct node *s=(struct node*)malloc(sizeof(struct node));
+  s->size=3;
+  s->top=-1;
+  s->arr=(int *)malloc(s->size*sizeof(int));
+  push(s,10);
+    push(s,20);
+      push(s,30);
+        push(s,40);
+}
